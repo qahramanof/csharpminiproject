aaf05332ecff7960208979c86a7505bb3a5f5417
unknown <kananng@code.edu.az>
2023-05-14 05:58:15 +0400

    is considered finished, the program.cs part remains

---
+++ CodeAcademyMiniProject/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+using CodeProject1.Business.Services;
+
+EmployeeService employeeService= new EmployeeService();
+DepartmentService departmentService= new DepartmentService();
+CompanyService companyService= new CompanyService();
+Console.WriteLine("hilton1");
+companyService.Create("Hilton");
+Console.WriteLine("hilton2");
+companyService.Create("Hilton");
